7a6914d549bb4be23228c7f3b4dbfb739b0e074c
Stepa <stpogood@gmail.com>
2025-08-31 19:24:26 +0300

    pre release

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,16 +1,17 @@
+// config.ts
 import "dotenv/config";
 import path from "path";
 import { fileURLToPath } from "url";
+import configJSON from "../../config.json" with { type: "json" };
 import pkg from "../../package.json" with { type: "json" };
-
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const root = path.join(__dirname, "../../..");
+const root = path.join(__dirname, "../..");
 const isDev = process.env.NODE_ENV === "development";
 
-type Config = {
+export type Config = {
   meta: {
     version: string;
     devMode: boolean;
@@ -20,6 +21,10 @@ type Config = {
     maxPasswordAttempts: number;
     logLevelConsole: string;
     language: string;
+    settings: {
+      cursorSymbol: string;
+      typingSpeed: number;
+    };
   };
   secrets: {
     telegramApiId: number;
@@ -44,6 +49,10 @@ const configBase = {
     maxPasswordAttempts: 5,
     logLevelConsole: process.env.LOG_LEVEL || "info",
     language: process.env.LANGUAGE || "en",
+    settings: {
+      cursorSymbol: configJSON.cursor_symbol,
+      typingSpeed: Number(configJSON.typing_speed),
+    },
   },
   secrets: {
     telegramApiId: Number(process.env.TELEGRAM_ID_API) || 0,
@@ -52,24 +61,36 @@ const configBase = {
   },
 };
 
-for (const [key, value] of Object.entries([
-  configBase.secrets.telegramApiId,
-  configBase.secrets.telegramApiHash,
-  configBase.secrets.telegramNumberPhone,
-])) {
+for (const [key, value] of Object.entries({
+  telegramApiId: configBase.secrets.telegramApiId,
+  telegramApiHash: configBase.secrets.telegramApiHash,
+  telegramNumberPhone: configBase.secrets.telegramNumberPhone,
+})) {
   if (value === "" || value === 0) {
     throw new Error(`Missing required environment variable: ${key}`);
   }
+}
+
+if (configBase.constants.settings.cursorSymbol.length !== 1) {
+  throw new Error("Cursor symbol must be a single character.");
+}
+
+if (
+  isNaN(configBase.constants.settings.typingSpeed) ||
+  configBase.constants.settings.typingSpeed <= 0.1 ||
+  configBase.constants.settings.typingSpeed >= 1
+) {
+  throw new Error("Typing speed must be a valid number (from 0.1 to 1).");
 }
 
 const paths = {
   root,
   tmp: path.join(root, ".tmp"),
   logs: path.join(root, ".logs"),
-  sessions: `.sessions_${configBase.secrets.telegramNumberPhone}`
+  sessions: `.sessions_${configBase.secrets.telegramNumberPhone}`,
 };
 
 export const config: Config = {
   ...configBase,
   paths,
-};
+} as const;
--- src/shared/utils/func.ts
+++ src/shared/utils/func.ts
@@ -1,5 +1,41 @@
+import { config } from "#config/config";
+import { logger } from "#shared/index";
 import { TelegramClient } from "telegram";
+import { NewMessage } from "telegram/events/index.js";
 
-export async function start(client: TelegramClient) {
-  console.log("Клиент успешно запущен!");
+export async function start(client: TelegramClient): Promise<void> {
+  const me = await client.getMe();
+
+  // Команда для печатания текста с анимацией
+  client.addEventHandler(
+    async (event) => {
+      if (!event.chatId) return;
+      if (!event.message) return;
+
+      const text = event.message.text.split("/p ")[1];
+      if (!text) return;
+      let typedText = "";
+
+      for (const char of text) {
+        typedText += char;
+        await client.editMessage(event.chatId, {
+          message: event.message.id,
+          text: typedText + config.constants.settings.cursorSymbol,
+        });
+        await new Promise(async (resolve) => setTimeout(resolve, await convert(config.constants.settings.typingSpeed)));
+      }
+      await client.editMessage(event.chatId, {
+        message: event.message.id,
+        text: typedText,
+      });
+    },
+    new NewMessage({ pattern: /^\/p (.+)$/ })
+  );
+
+  logger.info("Клиент запущен и слушает сообщения...");
+  client.disconnected;
+}
+
+async function convert(decimal: number) {
+  return decimal * 100;
 }
